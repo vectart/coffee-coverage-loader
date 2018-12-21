aebacc93ecac63469fe4965112aab03fd29df232
John Mortlock <john.mortlock@gmail.com>
2018-12-21 21:06:39 +1030

    Reuse the variable for test source

--- test.js
+++ test.js
@@ -85,7 +85,7 @@ describe('Coffee-coverage loader', function () {
 				file: '',
 				names: [],
 				mappings: ';;;;;;;;;EAAA,IAAA,MAAA,VAAA,FAAM,EACS,gBAAC,IAAD,lBAAC,IAAC,CAAA,OAAD,ZACZ,IAAC,CAAA,MAAM,bADI;mBAGb,OAAM,SAAC,MAAD;WACJ,IAAC,CAAA,GAAG,CAAC,IAAL,CAAU,IAAC,CAAA,OAAO,CAAA,YAAU,SAAO,IAAjB,CAAlB;EADI;;;;;;AAGF;;;;;;;kBACJ,OAAM,SAAA;IACJ,IAAC,CAAA,GAAG,CAAC,IAAL,CAAU,eAAV;WACA,gCAAM,CAAN;EAFI;;;;GADY;;AAKd;;;;;;;kBACJ,OAAM,SAAA;IACJ,IAAC,CAAA,GAAG,CAAC,IAAL,CAAU,cAAV;WACA,gCAAM,EAAN;EAFI;;;;GADY;;AAKpB,MAAM,IAAI,KAAJ,CAAU,kBAAV;;AACN,MAAM,IAAI,KAAJ,CAAU,oBAAV;;AAEN,GAAG,CAAC,IAAJ,CAAA;;AACA,GAAG,CAAC,IAAJ,CAAA',
-				sourcesContent: ["class Animal\n  constructor: (@name) ->\n    @log = []\n\n  move: (meters) ->\n    @log.push @name + \" moved #{meters}m.\"\n\nclass Snake extends Animal\n  move: ->\n    @log.push \"Slithering...\"\n    super 5\n\nclass Horse extends Animal\n  move: ->\n    @log.push \"Galloping...\"\n    super 45\n\nsam = new Snake \"Sammy the Python\"\ntom = new Horse \"Tommy the Palomino\"\n\nsam.move()\ntom.move()\n"],
+				sourcesContent: [testCoffeeSource],
 				sourceRoot: '',
 				sources: ['file.bin'],
 				version: 3
